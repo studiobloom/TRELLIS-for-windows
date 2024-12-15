0637520a8385e10e811e447b3c996d7177d4d6cc
bloom <108045000+studiobloom@users.noreply.github.com>
2024-12-15 12:59:36 -0800

    Server 100% Locally

--- app.py
+++ app.py
@@ -245,4 +245,5 @@ with gr.Blocks() as demo:
 if __name__ == "__main__":
     pipeline = TrellisImageTo3DPipeline.from_pretrained("./trellis/TRELLIS-image-large")
     pipeline.cuda()
-    demo.launch(share=True)
+    demo.launch(server_name="0.0.0.0", server_port=7860, share=False)
+
